cf473d998c97c58bdbccf4318092a49ed5ba02c6
NicoM <nico6mena@gmail.com>
2023-05-29 13:52:41 -0300

    Agrego modal perfil y articulo, cambio de css

--- html_css/MsPortal/js/menu.js
+++ html_css/MsPortal/js/menu.js
@@ -23,3 +23,37 @@ subMenuTitles.forEach((title) => {
     menu.classList.remove("submenu-active");
   });
 });
+
+/*MODAL PERFIL*/
+document.getElementById("openModal").addEventListener("click", function() {
+  document.getElementById("modal").style.display = "block";
+});
+
+document.getElementById("closeModal").addEventListener("click", function() {
+  document.getElementById("modal").style.display = "none";
+});
+
+window.addEventListener("click", function(event) {
+  if (event.target == document.getElementById("modal")) {
+    document.getElementById("modal").style.display = "none";
+  }
+});
+/*MODAL PERFIL FIN*/
+
+
+/*MODAL art*/
+document.getElementById("openModal-art").addEventListener("click", function() {
+  document.getElementById("modal-art").style.display = "block";
+});
+
+document.getElementById("closeModal-art").addEventListener("click", function() {
+  document.getElementById("modal-art").style.display = "none";
+});
+
+window.addEventListener("click", function(event) {
+  if (event.target == document.getElementById("modal-art")) {
+    document.getElementById("modal-art").style.display = "none";
+  }
+});
+
+/*MODAL art fin*/
